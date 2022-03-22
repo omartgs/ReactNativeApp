75e275f79db17680f9dd7685824cf88eb03671bb
omartgs <omar.jeina@treca-gimnazija.edu.ba>
2022-03-22 09:38:16 +0100

    Verzija2.0

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { GameEngine } from 'react-native-game-engine';
+import entities from './entities';
+import Physics from './physics';
 
 export default function App() {
+  const [running, setRunning] = useState(false)
+  useEffect(() => {
+    setRunning(true)
+  }, [])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1}}>
+
+      <GameEngine
+      systems={[Physics]}
+      entities={entities()}
+      running={running}
+      style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}
+      >
+
+      </GameEngine>
+      
+      <StatusBar style="auto" hidden={true} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
